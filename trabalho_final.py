23cf09a515133e0e6bd4a9990210b03ab2f79a40
Alberto Junior <alberto.gnj@gmail.com>
2026-05-27 13:48:46 -0300

    adicionado funcoes para calcular altura e distancia maxima

--- trabalho_final.py
+++ trabalho_final.py
@@ -11,27 +11,36 @@ def calcular_trajetoria(v_0, theta):
     while True:
         ponto=calcular_posicao(t, v_0, theta)
         if t>0 and ponto[1]<=0:
-            trajetoria.append(ponto)
             break
         else:
             trajetoria.append(ponto)
-            t+=0.1
+            t+=0.005
     return trajetoria
 
-"""
-def calcular_x_max():
+
+def calcular_x_max(v_0, theta):
+    g=9.81
+    x_max=((v_0**2)*np.sin(2*np.radians(theta)))/g
     return x_max
 
-def calcular_h_max():
+def calcular_h_max(v_0, theta):
+    g=9.81
+    h_max=((v_0*np.sin(np.radians(theta)))**2)/(2*g)
     return h_max
 
+"""
 def vec_velocidade():
     return velocidade
 """
 
-v_0=float(input("Informe a velocidade inicial: "))
-theta=float(input("Informe o ângulo de lançamento: "))
+v_0=float(input("Informe a velocidade inicial (m/s): "))
+theta=float(input("Informe o ângulo de lançamento (⁰): "))
 trajetoria=calcular_trajetoria(v_0, theta)
+x_max=calcular_x_max(v_0, theta)
+h_max=calcular_h_max(v_0, theta)
 
+print("[x, y]")
 for i in trajetoria:
-    print(f"{i[0]:.1f}, {i[1]:.1f}")
+    print(f"[{i[0]:.1f}, {i[1]:.1f}]")
+print(f"Distância máxima: {x_max:.1f} m")
+print(f"Altura máxima: {h_max:.1f} m")
